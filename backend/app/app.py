d49ecd24af64c8140268a6d5be29d60a6f2d0a18
El14sR <cantero.elias005@gmail.com>
2026-05-19 23:25:51 -0300

    feat: configurar CORS

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify
-
+from flask_cors import CORS
 from routes import menu_bp, reseñas_bp, login_bp, reservas_bp, servicios_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=["http://localhost:3000"])
+    # Registro de Blueprints
     app.register_blueprint(menu_bp)
     app.register_blueprint(reseñas_bp)
     app.register_blueprint(login_bp)
     app.register_blueprint(reservas_bp)
     app.register_blueprint(servicios_bp)
-    
+
+    # Manejo de errores
     @app.errorhandler(404)
     def page_not_found(e):
         return jsonify({"error": "Endpoint no encontrado"}), 404
