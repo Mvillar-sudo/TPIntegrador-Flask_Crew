a1aa9b3f09033852ead5621e82184fff1b455fe9
nprince <ngrinstin@fi.uba.ar>
2026-05-20 00:14:47 -0300

    Agrego endpoints adicionales para todas las routes de backend

--- backend/app/routes/menu.py
+++ backend/app/routes/menu.py
@@ -5,3 +5,15 @@ menu_bp = Blueprint('menu', __name__, url_prefix='/api/menu')
 @menu_bp.route('/', methods=['GET'])
 def get_menu():
     return jsonify({"mensaje": "Endpoint de Menú funcionando"})
+
+@menu_bp.route('/<int:id_plato>', methods=['GET'])
+def get_plato(id_plato): 
+    return jsonify({"mensaje": "Endpoint de plato encontrado"}) 
+
+@menu_bp.route('/', methods=['POST'])
+def crear_plato(): 
+    return jsonify({"mensaje": "Plato creado"}) 
+
+@menu_bp.route('/<int:id_plato>', methods=['PUT'])
+def modificar_plato(id_plato): 
+    return jsonify({"mensaje": "Endpoint de plato modificado"})
--- backend/app/routes/reseñas.py
+++ backend/app/routes/reseñas.py
@@ -5,3 +5,15 @@ reseñas_bp = Blueprint('reseñas', __name__, url_prefix='/api/resenas')
 @reseñas_bp.route('/', methods=['GET'])
 def get_resenas():
     return jsonify({"mensaje": "Endpoint de Reseñas funcionando"})
+
+@reseñas_bp.route('/<int:id_resena>', methods=['GET'])
+def get_resena(id_resena): 
+    return jsonify({"mensaje": "Endpoint de reseña encontrado"}) 
+
+@reseñas_bp.route('/', methods=['POST'])
+def crear_resena(): 
+    return jsonify({"mensaje": "reseña creado"}) 
+
+@reseñas_bp.route('/<int:id_resena>', methods=['PUT'])
+def modificar_resena(id_resena): 
+    return jsonify({"mensaje": "Endpoint de reseña modificado"})
