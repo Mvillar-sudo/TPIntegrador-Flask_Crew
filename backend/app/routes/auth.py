dcdca9fe110d6e5fd632c633f4be49a89f8191c5
El14sR <cantero.elias005@gmail.com>
2026-05-27 20:08:07 -0300

    refactor: cambiar importacion de login a auth (#34)

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 
-login_bp = Blueprint('login', __name__, url_prefix='/api/login')
+auth_bp = Blueprint('login', __name__, url_prefix='/api/login')
 
 @login_bp.route('/', methods=['GET', 'POST'])
 def login():
