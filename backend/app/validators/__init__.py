dc1e52aa745bb4068d34354b0a8c0ab27c3ef931
El14sR <cantero.elias005@gmail.com>
2026-06-05 12:38:34 -0300

    Agregar importaciones a archivo initpy #10

--- backend/app/validators/__init__.py
+++ backend/app/validators/__init__.py
@@ -1,5 +1,5 @@
-#from .menu_validator import
+from .admin_menu_validator import validar_id_plato, validar_crear_plato
 #from .reseñas_validator import
 #from .auth_validator import
 #from .reservas_validator import
-#from .servicio_validator import
+#from servicio_validator import
