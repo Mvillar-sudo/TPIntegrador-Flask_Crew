4dac6c40f16c6e66c4e37ac1adce12af1d5b7e01
nprince <ngrinstin@fi.uba.ar>
2026-06-06 16:33:10 -0300

    (#38) Borro archivos que causan conflictos de merge con rama hotfix/2

--- backend/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .menu import menu_bp
-from .reseñas import reseñas_bp
-from .auth import auth_bp
-from .reservas import reservas_bp
-from .servicio import servicio_bp
-from .dashboard import dashboard_bp
--- backend/app/validators/__init__.py
+++
@@ -1,5 +0,0 @@
-#from .menu_validator import
-#from .reseñas_validator import
-#from .auth_validator import
-#from .reservas_validator import
-#from .servicio_validator import
