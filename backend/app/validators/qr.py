56e7c9fb9a76d3eb74b2f46b6d0e779ba1f9aa69
valentin <valonso@fi.uba.ar>
2026-06-22 23:58:52 -0300

    soluciono problema de scaneo de qr

--- backend/app/validators/qr.py
+++ backend/app/validators/qr.py
@@ -1,27 +1,21 @@
 import qrcode
 import os
 import textwrap
+import json
 
 def generar_qr(id_reserva, nombre, fecha, hora, cantidad_personas, token_cancelacion):
     
-    # contenido que va adentro del QR
-    contenido = textwrap.dedent(f"""\
-        Reserva #{id_reserva}
-        Nombre: {nombre}
-        Fecha: {fecha}
-        Hora: {hora}
-        Personas: {cantidad_personas}
-        Token: {token_cancelacion}
-    """)
+    carpeta = "frontend/app/static/qr"
+    ruta = os.path.join(carpeta, f"reserva_{id_reserva}.png")
 
-    # generar el QR
+    contenido = json.dumps({
+        "id_reserva": id_reserva,
+        "qr_code": ruta
+    })
+
     qr = qrcode.make(contenido)
 
-    # carpeta donde se guardan los QR
-    carpeta = "app/static/qr"
-    os.makedirs(carpeta, exist_ok=True)  # crea la carpeta si no existe
-
-    ruta = os.path.join(carpeta, f"reserva_{id_reserva}.png")
+    os.makedirs(carpeta, exist_ok=True)
     qr.save(ruta)
 
     return ruta
