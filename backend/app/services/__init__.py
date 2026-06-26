02d4e6ae12c3a4f8bf9ed721f0c1f93b28da8978
Nicolas Grinstin <ngrinstin@fi.uba.ar>
2026-06-26 17:23:52 -0300

    (#75) Actualizo los init.py

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,5 +1,5 @@
 from .menu_service import crear_plato_service, obtener_menu_admin_service, obtener_plato_service, actualizar_parcial_plato_service, cambiar_estado_plato_service, eliminar_plato_service, obtener_menu_plato_service
 from .resenas_service import obtener_resenas,obtener_resena_id, crear_resena_db, eliminar_resena_db
 from .auth_service import post_register, post_login, obtener_usuarios, obtener_total_usuarios, obtener_usuario, eliminar_usuario, actualizar_usuario
-#from .reservas_service import
+from .reservas_service import obtener_reserva_service, obtener_total_reservas_pendientes, listar_reservas_service, cancelar_por_token_service, crear_reserva_service,  cancelar_reserva_service, validar_qr_service
 from .servicios_service import obtener_servicios, obtener_servicio_id, crear_servicio_db, actualizar_servicio_db, eliminar_servicio_db
--- backend/app/validators/__init__.py
+++ backend/app/validators/__init__.py
@@ -1,5 +1,4 @@
 from .menu_validator import validar_id_plato, validar_crear_plato
 from .resena_validator import validar_resena
 from .auth_validator import validar_login
-#from .reservas_validator import
 from .servicios_validator import validar_servicio
